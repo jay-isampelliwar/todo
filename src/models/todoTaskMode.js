67d06ea90de7bb5d98d81af3ef6905076aac3bb5
jay-isampelliwar <isampelliwar79@gmail.com>
2023-04-13 12:34:09 +0530

    updated todoModel

--- src/models/todoTaskMode.js
+++ src/models/todoTaskMode.js
@@ -19,28 +19,12 @@ const todoTaskModel = new Schema(
       type: String,
       required: true,
     },
-    // startDate: {
-    //   type: Date,
-    //   default: Date.now,
-    // },
-    // endDate: {
-    //   type: Date,
-    //   default: Date.now,
-    // },
-    // startTime: {
-    //   type: Date,
-    //   default: new Date().getTime(),
-    // },
-    // endTime: {
-    //   type: Date,
-    //   default: new Date().getTime(),
-    // },
     isDone: {
       type: Boolean,
       default: false,
     },
-  }
-  // { timestamps: true }
+  },
+  { timestamps: true }
 );
 
 module.exports = model("TodoTask", todoTaskModel);
