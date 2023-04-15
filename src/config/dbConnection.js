f780b646b061fb0d86717e15ff3e1196a1761160
jay-isampelliwar <isampelliwar79@gmail.com>
2023-04-15 15:14:41 +0530

    dpSTR update

--- src/config/dbConnection.js
+++ src/config/dbConnection.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose");
 
 const dbConnect = async () => {
   try {
-    const connect = mongoose.connect(process.env.CONNECTION_STRING);
+    const connect = mongoose.connect(
+      "mongodb+srv://jayisampelliwar:Jay112233@cluster1.6g97czu.mongodb.net/TODO_DB"
+    );
   } catch (err) {
     console.log(err);
   }
